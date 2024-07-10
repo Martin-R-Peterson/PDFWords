b652b232df0559fff100eb19bc2ee09f45588cc7
Martin-R-Peterson <martin.peterson.1993@gmail.com>
2024-07-10 19:21:15 +0200

    1

--- PDFWords/Program.cs
+++ PDFWords/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using App.Web;
 using App.Web.Data;
+using App.Web.Data.Repositories.Interfaces;
+using App.Web.Data.Repositories.Implementations;
+using App.Web.Services.Interfaces;
+using App.Web.Services.Implementations;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +13,10 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPdfRepository, PdfRepository>();
+builder.Services.AddScoped<IPdfService, PdfService>();
+builder.Services.AddScoped<IPdfParserService, PdfParserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
